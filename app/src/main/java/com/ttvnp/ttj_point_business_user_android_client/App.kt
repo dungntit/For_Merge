a920b15e11655315bced4c694f6a29ee2b55a9eb
Dzung Nguyen <admin@MacBook-Pro-cua-admin.local>
2018-04-13 17:40:18 +0700

    Update config

--- app/src/main/java/com/ttvnp/ttj_point_business_user_android_client/App.kt
+++ app/src/main/java/com/ttvnp/ttj_point_business_user_android_client/App.kt
@@ -1,9 +1,11 @@
 package com.ttvnp.ttj_point_business_user_android_client
 
 import android.util.Log
+import com.crashlytics.android.Crashlytics
 import com.ttvnp.ttj_point_business_user_android_client.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import io.fabric.sdk.android.Fabric
 import io.reactivex.plugins.RxJavaPlugins
 
 class App : DaggerApplication() {
@@ -16,6 +18,7 @@ class App : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
         setupRxErrorHandler()
+        Fabric.with(this, Crashlytics())
     }
 
     private fun setupRxErrorHandler() {
